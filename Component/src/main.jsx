dc49737039ebbb65a7a7861a3e26a81dd11f8055
Juan Esteban Ramirez Perdomo <1086418057@u.icesi.edu.co>
2024-02-01 18:41:41 -0500

    1

---
+++ Component/src/main.jsx
@@ -0,0 +1,63 @@
+import './style.css'
+import React from 'react'
+import {createRoot} from 'react-dom'
+
+const appContainer = document.getElementById("app")
+const root = createRoot(appContainer)
+
+
+function App (){
+  return(
+    <>
+    <Header></Header>
+    <Body></Body>
+    <Footer></Footer>
+    </>
+  )
+}
+
+// function App (){
+//   return[
+//     <Header></Header>,
+//     <Body></Body>,
+//     <Footer></Footer>
+//   ]
+// }
+
+function Header (){
+  return(
+    <header>
+    <h1>Main Page Title</h1>
+    <img src="" alt="" />  
+    </header>
+  )
+}
+
+function Body(){
+  return(
+    <body> 
+      <h1>
+        Principal Title
+      </h1>
+      <p>
+        Principal Paragrph Paragrph
+      </p>
+    </body>
+  )
+} 
+function Footer(){
+  return(
+    <footer>
+      <h3>
+        Good bye
+      </h3>
+      <img src = "" alt = ""/>
+    </footer>
+  )
+} 
+
+
+root.render(<App></App>)
+
+
+console.log(appContainer)
