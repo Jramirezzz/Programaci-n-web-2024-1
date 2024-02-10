4116c8381bea0da2b37166b7f159d548babfa724
juan esteban ramirez perdomo <juanestebanrp123@gmail.com>
2024-02-10 11:39:54 -0500

    carosuel

--- Component/src/Components/CarouseImg.jsx
+++ Component/src/Components/CarouseImg.jsx
@@ -7,22 +7,22 @@ import ArrowRigth from "../img/Rigth-Arrow.png"
 
 const DataImg= [
 {
-  id: "1",
-  img:"https://images.ctfassets.net/x7j9qwvpvr5s/4sTr4yELBNXLBl0O7agNm7/0a7a99acc36bb71401a6c8af955bd28d/Ducati-Panigale-Racing-_-World-Champion-Replica-2023-Limited-Edition-hero-1600x1000.jpg?fm=webp&q=90", 
+  
+  img:"https://images.ctfassets.net/x7j9qwvpvr5s/6OOP6qhp5jPrFwZkmt0DVo/273c4a520545cf23a3dbbb35c3668b9a/Multistrada-V4-Veicolo-Hero-short-1600x1000_7090-1.jpg?fm=webp&q=90", 
   Title:"MULTISTRADA V4S", 
   Description: "Ha llegado tu momento Ducati por 970.000 cop Mensuales", 
   Button:"MÁS INFORMACIÓN"
 },
 {
-  id: "2",
-  img:"https://images.ctfassets.net/x7j9qwvpvr5s/6OOP6qhp5jPrFwZkmt0DVo/273c4a520545cf23a3dbbb35c3668b9a/Multistrada-V4-Veicolo-Hero-short-1600x1000_7090-1.jpg?fm=webp&q=90", 
+  
+  img:"https://images.ctfassets.net/x7j9qwvpvr5s/4sTr4yELBNXLBl0O7agNm7/0a7a99acc36bb71401a6c8af955bd28d/Ducati-Panigale-Racing-_-World-Champion-Replica-2023-Limited-Edition-hero-1600x1000.jpg?fm=webp&q=90", 
   TItle:" DUCATI CORSE SPECIAL SERIES", 
   Description:"Panigale Racing season 2023 Edicion Limitada", 
   SecondDescription:"Cuatro Panigale V4 y una panigale V2 prooducidas en una serie limitada y enumerada para celebrar la temporada de récord", 
   Button:"DESCUBRIR MÁS"
 },
 {
-  id: "3",
+  
   img:"https://images.ctfassets.net/x7j9qwvpvr5s/4lITSt1AkuO5jp8I0cwf3D/9eb38c042f3c72d1b4860be5989d1d61/Ducati-DesertX-Rally-DWP24-Overview-hero-1600x1000.jpg?fm=webp&q=90", 
   Title:"DREAM WILDER", 
   Description:"DesertX Rally", 
@@ -44,17 +44,17 @@ export function Carousel  () {
     
       return (
         <div className="slider">
-          <img  className="left-arrow" onClick={prevSlide} src={ArrowLeft} alt="left-Arrow" />
-          <img className="right-arrow" onClick={nextSlide} src={ArrowRigth} alt="Rigth-Arrow"/>
+          <img  className="LeftArrow" onClick={prevSlide} src={ArrowLeft} alt="left-Arrow" />
+          <img className="RightArrow" onClick={nextSlide} src={ArrowRigth} alt="Rigth-Arrow"/>
           {
           DataImg.map((img, index) => {
             return current === index ? (
               <div key={index} className="slide">
-                <img src={img.img} alt="images" />
-                <h6>  {img.Title}</h6>
-                <h3>{img.Description}</h3>
-                <h5> {img.SecondDescription}</h5>
-                <button>{img.Button}</button>
+                <img className="ImgCarousel" src={img.img} alt="images" />
+                <h6 className="TitleCarousel">{img.Title}</h6>
+                <h3 className="DescriptionCarousel">{img.Description}</h3>
+                <h5 className="SecondDescriptionCarousel"> {img.SecondDescription}</h5>
+                <button className="ButtonCarousel" >{img.Button}</button>
               </div>
             ) : null;
           })}
