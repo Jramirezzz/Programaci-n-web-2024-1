b1f2a785838d0d09c231d1b2cac703be8c2c152b
juan esteban ramirez perdomo <juanestebanrp123@gmail.com>
2024-02-20 15:07:59 -0500

    Structure

--- Component/src/Components/CardType2.jsx
+++ Component/src/Components/CardType2.jsx
@@ -1,39 +1,33 @@
-import React from "react"
+import React from 'react'
 
 const CardInfo = [
-{
-  img: "https://images.ctfassets.net/x7j9qwvpvr5s/6krZBePA3g8nWMcONaqLdJ/e3a1e78d999aef52329c95712f7452ec/Ducati-configurator-prefooter-shortcuts-768x200.png?fm=webp&q=90", 
-  Title:"Configurador Ducati",
-},
-{
-  img: "https://images.ctfassets.net/x7j9qwvpvr5s/503HhZJCH3aR5OMPnYcreX/2578af1ba25edc8f53580567da8837ff/Scambler-configurator-prefooter-shortcuts-768x200.png?fm=webp&q=90", 
-  Title:"Configurador Scrambler",
-},
-{
-  img: "https://images.ctfassets.net/x7j9qwvpvr5s/343gkAolJy7TUzbmXW1qID/72d0091e57674ffb7f14dcca7e0fa365/Shop-Ducati-prefooter-shortcuts-768x200.png?fm=webp&q=90", 
-  Title:"Ducati Shop",
-},
+  {
+    img: 'https://images.ctfassets.net/x7j9qwvpvr5s/6krZBePA3g8nWMcONaqLdJ/e3a1e78d999aef52329c95712f7452ec/Ducati-configurator-prefooter-shortcuts-768x200.png?fm=webp&q=90',
+    Title: 'Configurador Ducati'
+  },
+  {
+    img: 'https://images.ctfassets.net/x7j9qwvpvr5s/503HhZJCH3aR5OMPnYcreX/2578af1ba25edc8f53580567da8837ff/Scambler-configurator-prefooter-shortcuts-768x200.png?fm=webp&q=90',
+    Title: 'Configurador Scrambler'
+  },
+  {
+    img: 'https://images.ctfassets.net/x7j9qwvpvr5s/343gkAolJy7TUzbmXW1qID/72d0091e57674ffb7f14dcca7e0fa365/Shop-Ducati-prefooter-shortcuts-768x200.png?fm=webp&q=90',
+    Title: 'Ducati Shop'
+  }
 
 ]
 
-export function CardType2 (){
-  return(
-    <div className="CardType2Container">
-    {CardInfo.map(({img,Title}) => { 
-  return(
-    
-        <div className="ContainerImgCardType2">
-            <img className="ImgCardType2"src ={img} alt="card"/>
-            <h4 className="TitleCardType2">{Title}</h4>
-        </div>
-  )
-    })}
+export function CardType2 () {
+  return (
+    <div className='CardType2Container'>
+      {CardInfo.map(({ img, Title }) => {
+        return (
+
+          <div className='ContainerImgCardType2'>
+            <img className='ImgCardType2' src={img} alt='card' />
+            <h4 className='TitleCardType2'>{Title}</h4>
+          </div>
+        )
+      })}
     </div>
   )
-    }
-    
-  
-    
-
-  
-  
+}
--- Component/src/Components/CardType4.jsx
+++ Component/src/Components/CardType4.jsx
@@ -1,45 +1,41 @@
-import React from "react"
+import React from 'react'
 
 const CardInfo = [
-{
-    img: "https://images.ctfassets.net/x7j9qwvpvr5s/3miKCrk3zsTmSPn6uWFQZY/236b4f1d9c577cc498fdb63dad61ed38/DWP23-Ep-02-ENG-MyDucati-Contenuto-Esclusivo-DCorse--663x249.png", 
-    Section:"MYDUCATI APP", 
-    Title:"MyDucati: Todo el Mundo Ducati en un solo lugar",
-    Description:"Suscríbete a MyDucati: descarga la app y descubre los contenidos exclusivos personalizados de la nueva gama 2023",
-    Link:"REGISTRARSE AHORA",
-},
-{
-  img:"https://images.ctfassets.net/x7j9qwvpvr5s/6PbpTdzjHif4BCX7y0R2tm/296f8b77eaf8423400a131c33cce5d04/DRE-Gift-Card-banner-wide-663x249.png", 
-  Section:"DRE ACADEMY", 
-  Title:"Una experiencia inolvidable: Cursos DRE Academy",
-  Description:"¿Qué mejor regalo para un apasionado de las motos que una experiencia de conducción en circuito o en off-road?",
-  Link:"DESCUBRIR MÁS",
-},
+  {
+    img: 'https://images.ctfassets.net/x7j9qwvpvr5s/3miKCrk3zsTmSPn6uWFQZY/236b4f1d9c577cc498fdb63dad61ed38/DWP23-Ep-02-ENG-MyDucati-Contenuto-Esclusivo-DCorse--663x249.png',
+    Section: 'MYDUCATI APP',
+    Title: 'MyDucati: Todo el Mundo Ducati en un solo lugar',
+    Description: 'Suscríbete a MyDucati: descarga la app y descubre los contenidos exclusivos personalizados de la nueva gama 2023',
+    Link: 'REGISTRARSE AHORA'
+  },
+  {
+    img: 'https://images.ctfassets.net/x7j9qwvpvr5s/6PbpTdzjHif4BCX7y0R2tm/296f8b77eaf8423400a131c33cce5d04/DRE-Gift-Card-banner-wide-663x249.png',
+    Section: 'DRE ACADEMY',
+    Title: 'Una experiencia inolvidable: Cursos DRE Academy',
+    Description: '¿Qué mejor regalo para un apasionado de las motos que una experiencia de conducción en circuito o en off-road?',
+    Link: 'DESCUBRIR MÁS'
+  }
 
 ]
 
-
-export function CardType4() {
+export function CardType4 () {
   return (
-    <div className="BigContainerCardType4">
-      {CardInfo.map(({img,Section,Title,Description,Link}) => {
+    <div className='BigContainerCardType4'>
+      {CardInfo.map(({ img, Section, Title, Description, Link }) => {
         return (
-          <div className="ContainerCardType4">
-            <div className="TextContainer">
-              <h6 className="SectionCardType4">{Section}</h6>
-              <h3 className="TitleCardType4">{Title}</h3>
-              <h5 className="DescriptionCardType4">{Description}</h5>
-              <button className="LinkCardType4">{Link}</button>
+          <div className='ContainerCardType4'>
+            <div className='TextContainer'>
+              <h6 className='SectionCardType4'>{Section}</h6>
+              <h3 className='TitleCardType4'>{Title}</h3>
+              <h5 className='DescriptionCardType4'>{Description}</h5>
+              <button className='LinkCardType4'>{Link}</button>
             </div>
-            <div className="ImageContainer">
-              <img style={{width:"500px",height:"270px",marginTop:"26px"}} className="ImgCardType4" src={img} alt="card" />
+            <div className='ImageContainer'>
+              <img style={{ width: '500px', height: '270px', marginTop: '26px' }} className='ImgCardType4' src={img} alt='card' />
             </div>
           </div>
-        );
+        )
       })}
     </div>
-  );
+  )
 }
-    
-
-
