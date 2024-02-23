d77f628725a4100c7969048ebdf4184078797157
juan esteban ramirez perdomo <juanestebanrp123@gmail.com>
2024-02-23 08:56:11 -0500

    Finished logic and html

--- Component/src/Components/NavBar/NavBar.jsx
+++ Component/src/Components/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ import { ModelNavBar } from '../ModelNavBar/ModelNavBar'
 import { HamMenu } from '../HamMenu/HamMenu'
 
 export function NavBar () {
+  const [isOpen, setIsOpen] = useState(false)
   const [showMenu2, setShowMenu2] = useState(false)
   const [showMenu, setShowMenu] = useState(false)
 
@@ -16,28 +17,48 @@ export function NavBar () {
   const HamClick = (event) => {
     setShowMenu2(!showMenu2)
   }
+
   return (
     <nav className='menu'>
       <section className='FirstNavBar'>
         <ul>
-          <button onClick={HamClick} className='ButtonLine'><img className='LogoMenu' style={{ width: '25px', height: '25px' }} src={logoMenu} /></button>
-          <li><button className='ButtonModel' onClick={handleClick}>MODELOS</button></li>
-          <li><a href='#'>EQUIPAMENTOS</a></li>
-          <li><a href='#'>PROMOCIONES</a></li>
-          <a><img style={{ width: '60px', height: '60px' }} src='https://assets.ducati.com/dist/0.16.2/assets/img/ducati_id.png' /></a>
+          <button
+            onClick={HamClick}
+            className='ButtonLine'
+          >
+            <img className='LogoMenu' style={{ width: '25px', height: '25px' }} src={logoMenu} />
+          </button>
+          <li>
+            <button className='ButtonModel' onClick={handleClick}>MODELOS</button>
+          </li>
+          <li>
+            <a href='#'>EQUIPAMENTOS</a>
+          </li>
+          <li>
+            <a href='#'>PROMOCIONES</a>
+          </li>
+          <a>
+            <img
+              style={{ width: '60px', height: '60px' }}
+              src='https://assets.ducati.com/dist/0.16.2/assets/img/ducati_id.png'
+            />
+          </a>
           <li><a href='#'>USO CERTIFICADO</a></li>
           <li><a href='#'>SOLICITAR TEST RIDE</a></li>
           <li><a href='#'>BUSCADOR CONSCESIONARIOS</a></li>
         </ul>
         <section className='ThirdNavBar'>
           {
-              !showMenu2 ? null : <HamMenu />
-            }
+             !isOpen && showMenu2 ? <HamMenu /> : showMenu ? <ModelNavBar /> : null
+
+          }
 
         </section>
       </section>
       <section className='SecondNavBar'>
-        {!showMenu ? null : <ModelNavBar />}
+        {/* {
+        !showMenu ? null : <ModelNavBar />
+        } */}
       </section>
     </nav>
   )
