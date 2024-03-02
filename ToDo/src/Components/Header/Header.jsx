5ad8d0f28a4589df6e990c3c6623d2a3ecd93870
juan esteban ramirez perdomo <juanestebanrp123mail.com>
2024-03-01 19:20:39 -0500

    Header

---
+++ ToDo/src/Components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export function Header (){
+    return (
+        <header>
+        <section>
+        <div className="header">
+            <h1> To  Do List </h1>
+        </div>
+        </section>
+        </header>
+    )
+}
