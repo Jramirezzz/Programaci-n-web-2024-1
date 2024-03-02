8d2bda7c869e6da26f1e6540eae45e875ae81496
juan esteban ramirez perdomo <juanestebanrp123mail.com>
2024-03-01 21:37:35 -0500

    styleForm

--- ToDo/src/Components/Form/Form.jsx
+++ ToDo/src/Components/Form/Form.jsx
@@ -1,14 +1,14 @@
 import { Button } from "../Buttons/Button";
-
+import './form.css'
 export function Form (){
     return (
         <section>
             <div className='container'>
-            <input 
+            <input
+                className="form" 
                 placeholder="Add a Task">
             </input>
             <Button Title = "Add Task"/>
-
             </div>
         </section>
     )
