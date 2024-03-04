35723be2d67bc5cdac5dcfa2187547df85e228c4
juan esteban ramirez perdomo <juanestebanrp123mail.com>
2024-03-04 17:13:33 -0500

    arrays

--- ToDo/src/Components/Footer/Footer.jsx
+++ ToDo/src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export function Footer (){
     return(
         <selection>
             <div className="footer">
-                <h5 className="TitleFooter"> 4 Tareas Coompletadas de 5 </h5>
+                <h5 className="TitleFooter"> 4 Tareas Completadas de 5 </h5>
                 <Button Title="Clear All Completed"/>
             </div>
         </selection>
--- ToDo/src/Components/Form/Form.jsx
+++ ToDo/src/Components/Form/Form.jsx
@@ -1,17 +1,48 @@
-import { Button } from "../Buttons/Button";
+import { useState } from "react";
+import {Button} from "../Buttons/Button"
 import './form.css'
+// import { useTypeButton } from "../../Hooks/UseTypeButton";
+
+
 export function Form (){
+    
+const [Task, SetTask] = useState([])
+const [taskInput, setTaskInput] = useState('')
+
+const handleTaksInput = (e) => {
+    setTaskInput(e.target.value);
+};
+
+const handleAddTaks = () => {
+    if(taskInput.trim() !== ''){
+        SetTask([...Task, taskInput]);
+        setTaskInput('');
+    }
+}
+
+
     return (
         <section>
+            <form>
             <div className='containerForm'>
             <input
-                className="form" 
-                placeholder="Add a Task">
+                 value={taskInput}
+                 onChange={handleTaksInput}
+                 className="form" 
+                 placeholder="Add a Task">
+
+                {
+                    console.log(Task)
+                }
+                    
+                
             </input>
-            <Button Title = "Add Task"/>
+            
+            <Button onClick={handleAddTaks} Title = "Add Task"/>
     
 
             </div>
+            </form>
         </section>
     )
 }
