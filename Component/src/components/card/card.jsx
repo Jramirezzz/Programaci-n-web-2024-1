532f98115c0a476a201ad79a1b630b6eb6fb2335
juan esteban ramirez perdomo <juanestebanrp123mail.com>
2024-04-07 18:00:46 -0500

    structure

---
+++ Component/src/components/card/card.jsx
@@ -0,0 +1,16 @@
+import './card.css'
+
+export function Card (){
+    return(
+        <section>
+            <div>
+        <img src="https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg" 
+             alt="Random cat" />
+            </div>
+            <div>
+                <p>Its this a random cat  or something else?</p>
+            </div>
+        </section>
+        
+    )
+}
