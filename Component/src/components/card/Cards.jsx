5d4f71625be2fc3754e028614ebc5c203c770baa
juan esteban ramirez perdomo <juanestebanrp123mail.com>
2024-04-10 14:09:27 -0500

    fetch

--- Component/src/components/card/Cards.jsx
+++ Component/src/components/card/Cards.jsx
@@ -4,8 +4,9 @@ import { Button } from "../button/button";
 import './card.css'
 
 export function CardCompleted(){
-    const [loading, setLoading] = useState(true)
-    const [Fact, setFact] = useState(null)
+    const [loading, setLoading] = useState(true);
+    const [Fact, setFact] = useState([]);
+    const [catImage, setCatImage] = useState(null);
 
     useEffect(() => {
         const fetchFacts = async () => {
@@ -17,7 +18,7 @@ export function CardCompleted(){
                 const data = await response.json();
                 console.log(data);
                 setFact([data]);
-                setLoading(false)// Almacenar el objeto de datos en un array
+                setLoading(false);
             } catch (error) {
                 console.error('Error al obtener los datos:', error);
             }
@@ -30,27 +31,51 @@ export function CardCompleted(){
             // Cancelar la solicitud si es necesario
         };
     }, [loading]);
+
+    useEffect(() => {
+        if (Fact.length > 0) {
+            const splitData = Fact[0].fact.split('').slice(0, 4).join('');
+            const ImageFacts = async () => {
+                try {
+                    const response = await fetch('https://cataas.com/cat/says/' + splitData);
+                    if (!response.ok) {
+                        throw new Error('No se pudo obtener los datos');
+                    }
+                    const Img = await response.json();
+                    console.log(Img);
+                    setCatImage(Img);
+                } catch (error) {
+                    console.error('Error al obtener los datos:', error);
+                }
+            };
+
+            ImageFacts();
+        }
+    }, [Fact]);
+
     const randomFact = () => {
         setLoading(true);
     };
+
     if(loading){
         return (
             <div className="fact">
                 <h2>Its loading</h2>
             </div>
         );
-
     }
-return (
-    <div>
-        <section>
-            <div>
-                {Fact.map((fact, id) => (
-                    <p key={id}>{fact.fact}</p>
-                ))}
-            </div>
-        </section>
-        <Button onClick={randomFact} title={"Another fact"}/>
-    </div>
-)
-} 
+
+    return (
+        <div>
+            <section>
+                <div>
+                    {Fact.map((fact, id) => (
+                        <p key={id}>{fact.fact}</p>
+                    ))}
+                </div>
+                <img src={catImage} alt="" />
+            </section>
+            <Button onClick={randomFact} title={"Another fact"}/>
+        </div>
+    );
+}
